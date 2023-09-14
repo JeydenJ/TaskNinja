fec49715bf71796a8822612a715c794c5fe0fc55
Jeyden Johnson <Jeyden.Johnson-1@student.uts.edu.au>
2023-09-14 18:50:13 +1000

    Initial Commit

---
+++ TaskNinja/TaskNinjaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskNinjaApp.swift
+//  TaskNinja
+//
+//  Created by Jeyden Johnson on 14/9/2023.
+//
+
+import SwiftUI
+
+@main
+struct TaskNinjaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
