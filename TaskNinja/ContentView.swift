e6241fdc593ee48765e7e9760d16396f6a34f753
Jeyden Johnson <Jeyden.Johnson-1@student.uts.edu.au>
2023-09-14 19:47:50 +1000

    added user interface for start page and some on main page

--- TaskNinja/ContentView.swift
+++ TaskNinja/ContentView.swift
@@ -8,14 +8,59 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var userName = ""
+    @State private var isNameEntered = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        if isNameEntered {
+            MainView(userName: userName)
+        } else {
+            VStack {
+                Image(systemName: "person.fill")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                Text("Welcome to TaskNinja!")
+                Text("Please enter your name:")
+                
+                TextField("Name", text: $userName)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+                
+                Button(action: {
+                    isNameEntered = true
+                }) {
+                    Text("Proceed")
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+            }
+            .padding()
         }
-        .padding()
+    }
+}
+
+struct MainView: View {
+    var userName: String
+    
+    var body: some View {
+        NavigationView {
+            VStack(alignment: .leading) {
+                Text("TaskNinja")
+                    .font(.largeTitle)
+                    .bold()
+                    .padding(.top)
+                
+                Text("Hello, \(userName)!")
+                    .font(.title)
+                    .padding(.top)
+                
+                Spacer()
+            }
+            .padding()
+            .navigationBarTitle("")
+        }
     }
 }
 
